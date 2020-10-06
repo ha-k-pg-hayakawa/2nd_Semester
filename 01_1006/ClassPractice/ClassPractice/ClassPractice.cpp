0be19a55a0bbf9f65b637071fd04d3709e3d23a0
MSI\haise <haise.game47@gmail.com>
2020-10-06 11:06:41 +0900

    クラスの作り方の復習用プロジェクトの作成

---
+++ 01_1006/ClassPractice/ClassPractice/ClassPractice.cpp
@@ -0,0 +1,38 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+
+class Fighter
+{
+public:
+	Fighter();
+	~Fighter();
+
+public:
+	int m_hp;
+	int m_str;
+	int m_mag;
+	int m_tec;
+	int m_spd;
+	int m_def;
+	int m_luc;
+	int m_mde;
+};
+
+Fighter::Fighter()
+{
+
+}
+
+Fighter::~Fighter()
+{
+
+}
+
+int main()
+{
+
+
+
+	system("pause");
+	return 0;
+}
