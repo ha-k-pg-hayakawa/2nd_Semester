e275d2e17b582c805bf3e39fd17a2536cd770d46
MSI\haise <haise.game47@gmail.com>
2020-10-06 14:27:15 +0900

    リファクタリングその１、 ・クラス名を変更 ・クラスの定義を別のcpp、hに移動 ・よく使うヘッダーファイルのインクルードを、Common.hに集約

--- 01_1006/ClassPractice/ClassPractice/ClassPractice.cpp
+++ 01_1006/ClassPractice/ClassPractice/ClassPractice.cpp
@@ -1,52 +1,19 @@
-﻿#include <stdlib.h>
-#include <stdio.h>
+﻿#include "Common.h"
+#include "Unit.h"
 
-class Fighter
-{
-public:
-	Fighter();
-	~Fighter();
-
-public:
-	int m_hp;
-	int m_str;
-	int m_mag;
-	int m_tec;
-	int m_spd;
-	int m_def;
-	int m_luc;
-	int m_mde;
-};
-
-Fighter::Fighter()
-{
-	m_hp=0;
-	m_str=0;
-	m_mag=0;
-	m_tec=0;
-	m_spd=0;
-	m_def=0;
-	m_luc=0;
-	m_mde=0;
-}
-
-Fighter::~Fighter()
-{
-
-}
 
 int main()
 {
-	Fighter Human;
+	Unit Fighter;
 
-	printf("hp  = %d\n", Human.m_hp);
-	printf("str = %d\n", Human.m_str);
-	printf("mag = %d\n", Human.m_mag);
-	printf("tec = %d\n", Human.m_tec);
-	printf("spd = %d\n", Human.m_spd);
-	printf("def = %d\n", Human.m_def);
-	printf("luc = %d\n", Human.m_luc);
-	printf("mde = %d\n", Human.m_mde);
+	printf("hp  = %d\n", Fighter.m_hp);
+	printf("str = %d\n", Fighter.m_str);
+	printf("mag = %d\n", Fighter.m_mag);
+	printf("tec = %d\n", Fighter.m_tec);
+	printf("spd = %d\n", Fighter.m_spd);
+	printf("def = %d\n", Fighter.m_def);
+	printf("luc = %d\n", Fighter.m_luc);
+	printf("mde = %d\n", Fighter.m_mde);
 
 	system("pause");
 	return 0;
