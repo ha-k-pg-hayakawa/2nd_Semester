55a1476a39d1e69cf2942e694a8392f0ba072b28
MSI\haise <haise.game47@gmail.com>
2020-10-13 10:12:33 +0900

    char型の文字列 string型の文字列

--- 02_1013/Practice02/Practice02_01_string/Practice02_01_string.cpp
+++ 02_1013/Practice02/Practice02_01_string/Practice02_01_string.cpp
@@ -1,10 +1,21 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#include <iostream>
 
 int main()
 {
+	char woad = 'a';
+	char name[] = "issei";
+	const char* cstr = "Hello";
 
+	printf("%c\n", woad);
+	printf("%s\n", name);
+	printf("%s\n", cstr);
+
+	std::string str = "文字列";
+
+	std::cout << str << ::exit;
 
 	system("pause");
 	return 0;
